913d167ef5e4ddce48e46ea50a82735a26ead0a0
Giacomo Guaresi <guaresi.giacomo@gmail.com>
2025-02-26 15:12:42 +0100

    add on going status

--- app/routes/production.py
+++ app/routes/production.py
@@ -5,10 +5,12 @@ from datetime import date
 
 production_bp = Blueprint("production", __name__)
 
+
 @production_bp.route('/ProductionOrder')
 def view_ProductionOrder():
     data = query_db('SELECT * FROM ProductionOrder')
     return render_template('ProductionOrder.html', data=data)
+
 
 @production_bp.route('/ProductionOrder/add', methods=['GET', 'POST'])
 def add_ProductionOrder():
@@ -20,7 +22,7 @@ def add_ProductionOrder():
         code = request.form['ProductCode']
         quantity = int(request.form['Quantity'])
 
-        # Inserisce la ProductionOrdere principale
+        # Inserisce la ProductionOrder principale
         cursor.execute(
             'INSERT INTO ProductionOrder (OrderDate, ProductCode, Quantity) VALUES (?, ?, ?)',
             (orderDate, code, quantity)
@@ -37,34 +39,16 @@ def add_ProductionOrder():
         for component_code, component_quantity in components:
             total_quantity = quantity * component_quantity
 
-            cursor.execute("SELECT * FROM Inventory WHERE ProductCode = ?", (component_code,))
-            item = cursor.fetchone()
-
-            if item is not None:
-                available = item[1] - item[2]
-                if available > total_quantity: 
-                    used = total_quantity
-                else:
-                    used = available
-            else: 
-                used = 0
-
             cursor.execute(
-                'INSERT INTO ProductionOrderProgress (OrderID, ProductCode, QuantityRequired, QuantityCompleted) VALUES (?, ?, ?, ?)',
-                (inserted_id, component_code, total_quantity, used)
+                'INSERT INTO ProductionOrderProgress (OrderID, ProductCode, QuantityRequired, QuantityCompleted) VALUES (?, ?, ?, 0)',
+                (inserted_id, component_code, total_quantity,)
             )
 
-            if used > 0:
-                cursor.execute(
-                    'UPDATE Inventory SET Locked = Locked + ? WHERE ProductCode = ?',
-                    (used, component_code,)
-                )
-            
             db.commit()
 
         db.commit()
         return redirect(url_for('production.view_ProductionOrder'))
-        
+
     orderDate = request.args.get('date', date.today().isoformat())
     code = request.args.get('code', '')
     quantity = request.args.get('quantity', '')
@@ -80,12 +64,54 @@ def delete_ProductionOrder(OrderID):
     query_db('DELETE FROM ProductionOrderProgress WHERE OrderID = ?', (OrderID,))
     return redirect(url_for('production.view_ProductionOrder'))
 
+
 @production_bp.route('/ProductionOrder/complete/<OrderID>')
 def complete_ProductionOrder(OrderID):
-    query_db('UPDATE ProductionOrder SET Status = ? WHERE OrderID = ?', ("Complete", OrderID))
+    query_db('UPDATE ProductionOrder SET Status = ? WHERE OrderID = ?',
+             ("Complete", OrderID))
     return redirect(url_for('production.view_ProductionOrder'))
+
 
 @production_bp.route('/ProductionOrder/ongoing/<OrderID>')
 def ongoing_ProductionOrder(OrderID):
-    query_db('UPDATE ProductionOrder SET Status = ? WHERE OrderID = ?', ("On Going", OrderID))
+    db = get_db()
+    cursor = db.cursor()
+    query_db('UPDATE ProductionOrder SET Status = ? WHERE OrderID = ?',
+             ("On Going", OrderID))
+
+    # Cerca i componenti nei progress
+    cursor.execute(
+        'SELECT * FROM ProductionOrderProgress WHERE OrderID = ?', (OrderID,))
+    all_progress = cursor.fetchall()
+
+    for progress in all_progress:
+        total_quantity = progress["QuantityRequired"] - progress["QuantityCompleted"]
+        productCode = progress["ProductCode"]
+        progressID = progress["ProgressID"]
+        cursor.execute(
+            "SELECT * FROM Inventory WHERE ProductCode = ?", (productCode,))
+        item = cursor.fetchone()
+
+        if item is not None:
+            available = item[1] - item[2]
+            if available > total_quantity:
+                used = total_quantity
+            else:
+                used = available
+        else:
+            used = 0
+
+        cursor.execute(
+            'UPDATE ProductionOrderProgress SET QuantityCompleted = QuantityCompleted + ? WHERE ProgressID = ?',
+            (used, progressID)
+        )
+
+        if used > 0:
+            cursor.execute(
+                'UPDATE Inventory SET Locked = Locked + ? WHERE ProductCode = ?',
+                (used, productCode,)
+            )
+
+        db.commit()
+
     return redirect(url_for('production.view_ProductionOrder'))
