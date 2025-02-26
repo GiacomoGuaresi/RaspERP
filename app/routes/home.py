340404da1c3c0a3d3a7eb90ff32555fe35fb2ac7
Giacomo Guaresi <guaresi.giacomo@gmail.com>
2025-02-26 17:34:50 +0100

    show logs into the pages

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,8 +1,17 @@
 # app/routes/home.py
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
+from app.utils import query_db
 
 home_bp = Blueprint("home", __name__)
+
 
 @home_bp.route("/")
 def index():
     return render_template("index.html")
+
+
+@home_bp.route("/logs")
+def logs():
+    data = query_db('SELECT * FROM Logs ORDER BY timestamp DESC LIMIT 50')
+    logs = [dict(row) for row in data]
+    return jsonify(logs)
