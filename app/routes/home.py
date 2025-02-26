444f86edca895cabc54c1110673fda116d793891
Giacomo Guaresi <guaresi.giacomo@gmail.com>
2025-02-26 18:59:22 +0100

    Add missing part to dashboard

--- app/routes/home.py
+++ app/routes/home.py
@@ -7,7 +7,23 @@ home_bp = Blueprint("home", __name__)
 
 @home_bp.route("/")
 def index():
-    return render_template("index.html")
+
+    ordersOnGoing = query_db("""SELECT ProductionOrderProgress.*, Product.Category 
+    FROM ProductionOrderProgress
+    JOIN ProductionOrder ON ProductionOrderProgress.OrderID = ProductionOrder.OrderID
+    JOIN Product ON ProductionOrderProgress.ProductCode = Product.ProductCode
+    WHERE Status = "On Going"
+    AND ProductionOrderProgress.QuantityRequired > ProductionOrderProgress.QuantityCompleted""")
+
+    ordersPlanned = query_db("""SELECT ProductionOrderProgress.*, Product.Category, Product.Image 
+    FROM ProductionOrderProgress
+    JOIN ProductionOrder ON ProductionOrderProgress.OrderID = ProductionOrder.OrderID
+    JOIN Product ON ProductionOrderProgress.ProductCode = Product.ProductCode
+    WHERE Status = "Planned"
+    AND ProductionOrderProgress.QuantityRequired > ProductionOrderProgress.QuantityCompleted""")
+
+
+    return render_template("index.html", ordersOnGoing=ordersOnGoing, ordersPlanned=ordersPlanned)
 
 
 @home_bp.route("/logs")
