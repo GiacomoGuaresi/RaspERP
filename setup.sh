34da23cfaa7c87a74bf4a21ff0488efb730ee56f
Giacomo Guaresi <guaresi.giacomo@gmail.com>
2025-02-26 23:01:41 +0100

    fix steup.sh

--- setup.sh
+++ setup.sh
@@ -1,40 +1,50 @@
 #!/bin/bash
 
 echo "Updating system and installing required packages..."
-sudo apt update && sudo apt install -y python3 python3-pip chromium-browser unclutter
+sudo apt update && sudo apt install -y python3 python3-pip chromium-browser unclutter sqlite3
 
 echo "Installing Python dependencies..."
 pip3 install -r requirements.txt
 
 echo "Creating Flask systemd service..."
-cat <<EOF | sudo tee /etc/systemd/system/flask_app.service
+cat <<EOF | sudo tee /etc/systemd/system/RaspERP.service
 [Unit]
-Description=Flask App
+Description=RaspERP
 After=network.target
 
 [Service]
-User=pi
-WorkingDirectory=/home/pi/myflaskapp
-ExecStart=/usr/bin/python3 /home/pi/myflaskapp/run.py
+User=root
+WorkingDirectory=/home/pi/RaspERP
+ExecStart=/usr/bin/python3 /home/pi/RaspERP/run.py
 Restart=always
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-echo "Enabling Flask service..."
+echo "Enabling RaspERP service..."
 sudo systemctl daemon-reload
-sudo systemctl enable flask_app
-sudo systemctl start flask_app
+sudo systemctl enable RaspERP
+sudo systemctl start RaspERP
+
+
 
 echo "Configuring Chromium kiosk mode..."
-mkdir -p ~/.config/lxsession/LXDE-pi/
-cat <<EOF >> ~/.config/lxsession/LXDE-pi/autostart
-@xset s off
-@xset -dpms
-@xset s noblank
-@chromium-browser --noerrdialogs --disable-infobars --kiosk http://127.0.0.1:5000/Inventory/codereader
-EOF
+# TODO: creare un modo per autostartare chromium in modalità kiosk
 
-echo "Setup complete! Rebooting now..."
-sudo reboot
+echo "Setting up SQLite database..."
+
+DB_PATH="/home/pi/RaspERP/database.db"
+SQL_TEMPLATE="/home/pi/RaspERP/db_template.sql"
+
+if [ ! -f "$DB_PATH" ]; then
+    echo "Creating database from template..."
+    sudo sqlite3 "$DB_PATH" < "$SQL_TEMPLATE"
+    echo "Database created successfully!"
+else
+    echo "Database already exists, skipping creation."
+fi
+
+
+# echo "Setup complete! Rebooting now..."
+# sudo reboot
