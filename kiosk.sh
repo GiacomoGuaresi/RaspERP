03e0d488bb54e1d738a95290db47d1421ffd2384
Giacomo Guaresi <guaresi.giacomo@gmail.com>
2025-02-27 21:47:10 +0100

    fix kiosk url

--- kiosk.sh
+++ kiosk.sh
@@ -1,2 +1,2 @@
 sleep 4
-/bin/chromium-browser  --kiosk  --ozone-platform=wayland --start-maximized https://localhost/Inventory/codereader &
+/bin/chromium-browser  --kiosk  --ozone-platform=wayland --start-maximized http://localhost/Inventory/codereader &
