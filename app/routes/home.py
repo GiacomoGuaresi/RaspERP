1bd286afec6ec70db8eaa7add997d8badde4a1fb
Giacomo Guaresi <guaresi.giacomo@gmail.com>
2025-03-13 11:33:14 +0100

    refactoring dashboard

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,6 +1,7 @@
 # app/routes/home.py
 from flask import Blueprint, render_template, jsonify
 from app.utils import query_db
+from collections import defaultdict
 
 home_bp = Blueprint("home", __name__)
 
@@ -8,22 +9,31 @@ home_bp = Blueprint("home", __name__)
 @home_bp.route("/")
 def index():
 
-    ordersOnGoing = query_db("""SELECT ProductionOrderProgress.*, Product.Category 
+    ordersOnGoing = query_db("""SELECT ProductionOrderProgress.*, Product.Category, ProductionOrder.ProductCode as OrderProductCode 
     FROM ProductionOrderProgress
     JOIN ProductionOrder ON ProductionOrderProgress.OrderID = ProductionOrder.OrderID
     JOIN Product ON ProductionOrderProgress.ProductCode = Product.ProductCode
     WHERE Status = "On Going"
     AND ProductionOrderProgress.QuantityRequired > ProductionOrderProgress.QuantityCompleted""")
 
-    ordersPlanned = query_db("""SELECT ProductionOrderProgress.*, Product.Category, Product.Image 
+    ordersPlanned = query_db("""SELECT ProductionOrderProgress.*, Product.Category, Product.Image, ProductionOrder.ProductCode as OrderProductCode 
     FROM ProductionOrderProgress
     JOIN ProductionOrder ON ProductionOrderProgress.OrderID = ProductionOrder.OrderID
     JOIN Product ON ProductionOrderProgress.ProductCode = Product.ProductCode
     WHERE Status = "Planned"
     AND ProductionOrderProgress.QuantityRequired > ProductionOrderProgress.QuantityCompleted""")
 
+    # Raggruppa ordersOnGoing per OrderProductCode
+    grouped_orders_ongoing = defaultdict(list)
+    for row in ordersOnGoing:
+        grouped_orders_ongoing[row["OrderProductCode"]].append(row)
 
-    return render_template("index.html", ordersOnGoing=ordersOnGoing, ordersPlanned=ordersPlanned)
+    # Raggruppa ordersPlanned per OrderProductCode
+    grouped_orders_planned = defaultdict(list)
+    for row in ordersPlanned:
+        grouped_orders_planned[row["OrderProductCode"]].append(row)
+
+    return render_template("index.html", grouped_orders_ongoing=grouped_orders_ongoing, grouped_orders_planned=grouped_orders_planned)
 
 
 @home_bp.route("/logs")
