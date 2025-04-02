a952ce372dd3c121c25e8fe0d2b5da8873fcc0ba
Giacomo Guaresi <guaresi.giacomo@gmail.com>
2025-04-02 16:18:37 +0200

    add new kpi to dashboard

--- app/routes/home.py
+++ app/routes/home.py
@@ -8,16 +8,44 @@ home_bp = Blueprint("home", __name__)
 
 @home_bp.route("/")
 def index():
+    # ProductionOrder count
     data = {}
-
     if get_current_user() is not None and get_current_user() != "":
         data = query_db('SELECT count(1) as productionOrderNumber FROM ProductionOrder LEFT JOIN Product ON Product.ProductCode = ProductionOrder.ProductCode WHERE AssignedUser = ?', (get_current_user(),))
     else:
-        data = query_db('SELECT count(1) as productionOrderNumber FROM ProductionOrder LEFT JOIN Product ON Product.ProductCode = ProductionOrder.ProductCode')
-    
+        data = query_db(
+            'SELECT count(1) as productionOrderNumber FROM ProductionOrder LEFT JOIN Product ON Product.ProductCode = ProductionOrder.ProductCode')
     productionOrder = data[0]["productionOrderNumber"]
+    def get_missing_parts_by_category(category):
+        try:
+            result = query_db(f"""
+                SELECT SUM(ProductionOrderProgress.QuantityRequired) - SUM(ProductionOrderProgress.QuantityCompleted) as MissingParts
+                FROM ProductionOrderProgress
+                JOIN ProductionOrder ON ProductionOrderProgress.OrderID = ProductionOrder.OrderID
+                JOIN Product ON ProductionOrderProgress.ProductCode = Product.ProductCode
+                WHERE ProductionOrder.Status = "On Going"
+                AND Product.Category = "{category}"
+            """)
+            if result and result[0] and result[0]["MissingParts"] is not None:
+                return result[0]["MissingParts"]
+            else:
+                return 0
+        except IndexError:
+            return 0
 
-    return render_template("index.html", productionOrder=productionOrder)
+    # MissingParts / PrintedParts
+    missingPartsPrintedPart = get_missing_parts_by_category("PrintedPart")
+    print(missingPartsPrintedPart)
+
+    # MissingParts / Component
+    missingPartsComponent = get_missing_parts_by_category("Component")
+    print(missingPartsComponent)
+
+    # MissingParts / Subassembly
+    missingPartsSubassembly = get_missing_parts_by_category("Subassembly")
+    print(missingPartsSubassembly)
+
+    return render_template("index.html", productionOrder=productionOrder, missingPartsPrintedPart=missingPartsPrintedPart, missingPartsComponent=missingPartsComponent, missingPartsSubassembly=missingPartsSubassembly)
 
 
 @home_bp.route("/logs")
