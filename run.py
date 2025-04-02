031c922421036347c602969b631cd7d25d25d207
Giacomo Guaresi <guaresi.giacomo@gmail.com>
2025-04-02 09:39:22 +0200

    add step progress

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    if len(sys.argv) >= 2 and sys.argv[1] == "--DEBUG":
+    if len(sys.argv) >= 2 and sys.argv[1].upper() == "--DEV":
         app.run(debug=True)
     else: 
         app.run(host='0.0.0.0', port=80)
