fe594a1cd40692bc700e018cf0fd3d638cf4335a
Giacomo Guaresi <guaresi.giacomo@gmail.com>
2025-05-23 15:31:19 +0200

    feat: enhance production order increase logic and update inventory handling

--- app/routes/production.py
+++ app/routes/production.py
@@ -62,6 +62,7 @@ def delete_ProductionOrder(OrderID):
     delete_ProductionOrder_recursive(OrderID)
     return redirect(url_for('production.view_ProductionOrder'))
 
+
 @production_bp.route('/ProductionOrder/ongoing/<OrderID>')
 @login_required
 def ongoing_ProductionOrder(OrderID):
@@ -112,44 +113,50 @@ def ongoing_ProductionOrder(OrderID):
 @production_bp.route('/ProductionOrder/increase/<int:OrderID>')
 @login_required
 def increase_ProductionOrder(OrderID):
-    db = get_db()
-    cursor = db.cursor()
-
-    cursor.execute(
-        'SELECT QuantityCompleted, Quantity FROM ProductionOrder WHERE OrderID = ?',
+    rows = query_db('SELECT * FROM ProductionOrder WHERE OrderID = ?',
         (OrderID,))
-    row = cursor.fetchone()
+    row = rows[0] if rows else None
 
     if row:
-        quantity_present, quantity_required = row
-        if quantity_present < quantity_required:
-            cursor.execute(
-                'UPDATE ProductionOrder SET QuantityCompleted = QuantityCompleted + 1 WHERE OrderID = ?',
-                (OrderID,)
-            )
-            db.commit()
-            productCode = query_db(
+        if row["QuantityCompleted"] < row["Quantity"]:
+
+            query_db('UPDATE ProductionOrder SET QuantityCompleted = QuantityCompleted + 1 WHERE OrderID = ?',
+                (OrderID,))
+            
+            query_db('UPDATE ProductionOrderProgress SET QuantityCompleted = QuantityCompleted + 1 WHERE OrderID = ? AND ProductCode = ?',
+                (row["ParentOrderID"], row["ProductCode"]))
+                     
+            productCode=query_db(
                 'SELECT ProductCode FROM ProductionOrder WHERE OrderID = ?', (OrderID,))[0]["ProductCode"]
             increase_Inventory(productCode, 1)
-            if (quantity_present + 1) == quantity_required:
-                cursor.execute(
+            if (row["QuantityCompleted"] + 1) == row["Quantity"]:
+                query_db(
                     'UPDATE ProductionOrder SET Status = ? WHERE OrderID = ?',
                     ("Complete", OrderID)
                 )
-                db.commit()
+                # Remove Locked Inventory parts
+                all_progress=query_db(
+                    'SELECT * FROM ProductionOrderProgress WHERE OrderID = ?', (OrderID,))
+                for progress in all_progress:
+                    query_db(
+                        'UPDATE Inventory SET Locked = Locked - ?, QuantityOnHand = QuantityOnHand - ? WHERE ProductCode = ?',
+                        (progress["QuantityCompleted"],
+                         progress["QuantityCompleted"],
+                         progress["ProductCode"],)
+                    )
 
     return redirect(url_for('progress.view_ProductionOrderProgress', OrderID=OrderID))
 
 
-@production_bp.route('/ProductionOrder/decrease/<int:OrderID>')
-@login_required
+@ production_bp.route('/ProductionOrder/decrease/<int:OrderID>')
+@ login_required
 def decrease_ProductionOrder(OrderID):
-    db = get_db()
-    cursor = db.cursor()
+    db=get_db()
+    cursor=db.cursor()
 
     cursor.execute(
         'SELECT QuantityCompleted FROM ProductionOrder WHERE OrderID = ?', (OrderID,))
-    row = cursor.fetchone()
+    row=cursor.fetchone()
 
     if row and row["QuantityCompleted"] > 0:
         cursor.execute(
@@ -168,25 +175,25 @@ def decrease_ProductionOrder(OrderID):
 
 
 def add_ProductionOrder_process(orderDate, code, quantity, parentOrderID, assignedUser, status):
-    db = get_db()
-    cursor = db.cursor()
+    db=get_db()
+    cursor=db.cursor()
 
     # Inserisce la ProductionOrder principale
     cursor.execute(
         'INSERT INTO ProductionOrder (OrderDate, ProductCode, Quantity, ParentOrderID, AssignedUser, Status) VALUES (?, ?, ?, ?, ?, ?)',
         (orderDate, code, quantity, parentOrderID, assignedUser, status)
     )
-    inserted_id = cursor.lastrowid
+    inserted_id=cursor.lastrowid
 
     # Cerca i componenti nella BillOfMaterials
     cursor.execute(
         'SELECT ChildProductCode, Quantity FROM BillOfMaterials WHERE ProductCode = ?',
         (code,)
     )
-    components = cursor.fetchall()
+    components=cursor.fetchall()
 
     for component_code, component_quantity in components:
-        total_quantity = quantity * component_quantity
+        total_quantity=quantity * component_quantity
         cursor.execute(
             'INSERT INTO ProductionOrderProgress (OrderID, ProductCode, QuantityRequired, QuantityCompleted) VALUES (?, ?, ?, 0)',
             (inserted_id, component_code, total_quantity,)
@@ -198,28 +205,28 @@ def add_ProductionOrder_process(orderDate, code, quantity, parentOrderID, assign
 
 
 def addAllSub_ProductionOrder(OrderID):
-    baseOrderInfo = query_db(
+    baseOrderInfo=query_db(
         """select * from ProductionOrder where OrderID = ?""", (OrderID,))
-    orderDate = baseOrderInfo[0]["OrderDate"]
-    status = baseOrderInfo[0]["Status"]
+    orderDate=baseOrderInfo[0]["OrderDate"]
+    status=baseOrderInfo[0]["Status"]
 
     def addAllSub_ProductionOrder_recursive(OrderID, orderDate, status):
-        data = query_db("""SELECT *
+        data=query_db("""SELECT *
             FROM ProductionOrderProgress
             JOIN Product ON ProductionOrderProgress.ProductCode = Product.ProductCode
             LEFT JOIN ProductionOrder ON ProductionOrder.ParentOrderID = ProductionOrderProgress.OrderID AND ProductionOrder.ProductCode = ProductionOrderProgress.ProductCode
             WHERE ProductionOrderProgress.OrderID = ? AND Category = "Subassembly" and ProductionOrder.OrderID IS null""", (OrderID,))
         for row in data:
-            metadata = json.loads(row["Metadata"])
+            metadata=json.loads(row["Metadata"])
 
-            code = row["ProductCode"]
-            quantity = row["QuantityRequired"] - row["QuantityCompleted"]
-            parentOrderID = OrderID
-            assignedUser = metadata.get("DefaultUser", "")
+            code=row["ProductCode"]
+            quantity=row["QuantityRequired"] - row["QuantityCompleted"]
+            parentOrderID=OrderID
+            assignedUser=metadata.get("DefaultUser", "")
 
             log_action("Create Suborder for " + code)
             print("Create Suborder for " + code)
-            inserted_id = add_ProductionOrder_process(
+            inserted_id=add_ProductionOrder_process(
                 orderDate, code, quantity, parentOrderID, assignedUser, status)
 
             if (status == "On Going"):
